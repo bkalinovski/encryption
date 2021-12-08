953525377f1a435814bd4f6082ca6c85e21ca3c6
bkalinovski <bkalinovski@starnet.md>
2021-12-08 17:07:58 +0200

    Update RsaEncryptionService.cs

--- src/Encryption/Services/RsaEncryptionService.cs
+++ src/Encryption/Services/RsaEncryptionService.cs
@@ -1,21 +1,32 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
 using Encryption.Contract;
 
 namespace Encryption.Services
 {
     public class RsaEncryptionService : IAsymmetricEncryption
     {
+        private RSACryptoServiceProvider rsaService;
         public RsaEncryptionService()
         {
+            rsaService = new RSACryptoServiceProvider();
         }
 
         public string Encrypt(string message, string publicKey)
         {
-            throw new System.NotImplementedException();
+            //rsaService.ImportParameters(GetRSAParameters(publicKey));
+            var data = Encoding.Unicode.GetBytes(message);
+            var cypher = rsaService.Encrypt(data, false);
+            return Convert.ToBase64String(cypher);
         }
 
         public string Decrypt(string message, string privateKey)
         {
-            throw new System.NotImplementedException();
+            //rsaService.ImportParameters(GetRSAParameters(privateKey));
+            var data = Encoding.Unicode.GetBytes(message);
+            var cypher = rsaService.Decrypt(data, false);
+            return Convert.ToBase64String(cypher);
         }
     }
 }
