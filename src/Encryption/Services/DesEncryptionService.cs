44ee63c137da58a393cbb6621c89e9dbe49615d4
bkalinovski <bkalinovski@starnet.md>
2021-12-29 14:55:10 +0200

    beautify code

--- src/Encryption/Services/DesEncryptionService.cs
+++ src/Encryption/Services/DesEncryptionService.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using Encryption.Contract;
@@ -10,57 +9,44 @@ namespace Encryption.Services
     {
         public string Encrypt(string message, string password)
         {
-            byte[] myEncryptedArray = UTF8Encoding.UTF8.GetBytes(message);
+            var myEncryptedArray = Encoding.UTF8.GetBytes(message);
+            var myMd5CryptoService = new MD5CryptoServiceProvider();
+            var mySecurityKeyArray = myMd5CryptoService.ComputeHash(Encoding.UTF8.GetBytes(password));
+            myMd5CryptoService.Clear();
 
-            var MyMD5CryptoService = new MD5CryptoServiceProvider();
-
-            byte[] MysecurityKeyArray = MyMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(password));
-
-            MyMD5CryptoService.Clear();
-
-            var myTripleDesCryptoService = new TripleDESCryptoServiceProvider();
-
-            myTripleDesCryptoService.Key = MysecurityKeyArray;
-
-            myTripleDesCryptoService.Mode = CipherMode.ECB;
-
-            myTripleDesCryptoService.Padding = PaddingMode.PKCS7;
-
-            var myCrytpoTransform = myTripleDesCryptoService.CreateEncryptor();
-
-            byte[] myresultArray = myCrytpoTransform.TransformFinalBlock(myEncryptedArray, 0, myEncryptedArray.Length);
-
+            var myTripleDesCryptoService = new TripleDESCryptoServiceProvider
+            {
+                Key = mySecurityKeyArray, 
+                Mode = CipherMode.ECB, 
+                Padding = PaddingMode.PKCS7
+            };
+            
+            var myCryptoTransform = myTripleDesCryptoService.CreateEncryptor();
+            var myResultArray = myCryptoTransform.TransformFinalBlock(myEncryptedArray, 0, myEncryptedArray.Length);
             myTripleDesCryptoService.Clear();
 
-            return Convert.ToBase64String(myresultArray, 0,
-                myresultArray.Length);
+            return Convert.ToBase64String(myResultArray, 0, myResultArray.Length);
         }
 
         public string Decrypt(string message, string password)
         {
-            byte[] myDecryptArray = Convert.FromBase64String(message);
+            var myDecryptArray = Convert.FromBase64String(message);
+            var myMd5CryptoService = new MD5CryptoServiceProvider();
+            var mySecurityKeyArray = myMd5CryptoService.ComputeHash(Encoding.UTF8.GetBytes(password));
+            myMd5CryptoService.Clear();
 
-            var MyMD5CryptoService = new MD5CryptoServiceProvider();
-
-            byte[] MysecurityKeyArray = MyMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(password));
-
-            MyMD5CryptoService.Clear();
-
-            var MyTripleDESCryptoService = new TripleDESCryptoServiceProvider();
-
-            MyTripleDESCryptoService.Key = MysecurityKeyArray;
-
-            MyTripleDESCryptoService.Mode = CipherMode.ECB;
-
-            MyTripleDESCryptoService.Padding = PaddingMode.PKCS7;
-
-            var MyCrytpoTransform = MyTripleDESCryptoService.CreateDecryptor();
-
-            byte[] MyresultArray = MyCrytpoTransform.TransformFinalBlock(myDecryptArray, 0, myDecryptArray.Length);
-
-            MyTripleDESCryptoService.Clear();
-
-            return UTF8Encoding.UTF8.GetString(MyresultArray);
+            var myTripleDesCryptoService = new TripleDESCryptoServiceProvider
+            {
+                Key = mySecurityKeyArray, 
+                Mode = CipherMode.ECB, 
+                Padding = PaddingMode.PKCS7
+            };
+            
+            var myCryptoTransform = myTripleDesCryptoService.CreateDecryptor();
+            var myResultArray = myCryptoTransform.TransformFinalBlock(myDecryptArray, 0, myDecryptArray.Length);
+            myTripleDesCryptoService.Clear();
+            
+            return Encoding.UTF8.GetString(myResultArray);
         }
     }
 }
