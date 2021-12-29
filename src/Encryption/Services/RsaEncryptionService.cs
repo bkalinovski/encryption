aed11001f08b46d01fc30a262f63f2a6cc0ff60d
bkalinovski <bkalinovski@starnet.md>
2021-12-29 14:45:44 +0200

    Update RsaEncryptionService.cs

--- src/Encryption/Services/RsaEncryptionService.cs
+++ src/Encryption/Services/RsaEncryptionService.cs
@@ -1,32 +1,53 @@
 using System;
+using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using Encryption.Contract;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
 
 namespace Encryption.Services
 {
     public class RsaEncryptionService : IAsymmetricEncryption
     {
-        private RSACryptoServiceProvider rsaService;
-        public RsaEncryptionService()
-        {
-            rsaService = new RSACryptoServiceProvider();
-        }
-
         public string Encrypt(string message, string publicKey)
         {
-            //rsaService.ImportParameters(GetRSAParameters(publicKey));
-            var data = Encoding.Unicode.GetBytes(message);
-            var cypher = rsaService.Encrypt(data, false);
-            return Convert.ToBase64String(cypher);
+            var rsaPublicKey = ImportPublicKey(publicKey);
+            var bytesPlainTextData = Encoding.Unicode.GetBytes(message);
+            var bytesCypherText = rsaPublicKey.Encrypt(bytesPlainTextData, false);
+            return Convert.ToBase64String(bytesCypherText);
         }
 
         public string Decrypt(string message, string privateKey)
         {
-            //rsaService.ImportParameters(GetRSAParameters(privateKey));
-            var data = Encoding.Unicode.GetBytes(message);
-            var cypher = rsaService.Decrypt(data, false);
-            return Convert.ToBase64String(cypher);
+            var rsaPrivateKey = ImportPrivateKey(privateKey);
+            var bytesCypherText = Convert.FromBase64String(message);
+            var bytesPlainTextData = rsaPrivateKey.Decrypt(bytesCypherText, false);
+            return Encoding.Unicode.GetString(bytesPlainTextData);
+        }
+        
+        private static RSACryptoServiceProvider ImportPrivateKey(string pem)
+        {
+            var pr = new PemReader(new StringReader(pem));
+            var keyPair = (AsymmetricCipherKeyPair)pr.ReadObject();
+            var rsaParams = DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)keyPair.Private);
+
+            var csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(rsaParams);
+            return csp;
+        }
+        
+        private static RSACryptoServiceProvider ImportPublicKey(string pem)
+        {
+            var pr = new PemReader(new StringReader(pem));
+            var publicKey = (AsymmetricKeyParameter)pr.ReadObject();
+            var rsaParams = DotNetUtilities.ToRSAParameters((RsaKeyParameters)publicKey);
+
+            var csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(rsaParams);
+            return csp;
         }
     }
 }
